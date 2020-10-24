17f7400dfc58a73f1f4d9b6ca578a8bcd0271d74
Juan Velez Ballesteros <juaball@amazon.com>
2020-10-24 11:51:57 -0500

    clase angular

--- proyecto-clase/projects/alumnos-app/src/app/app-routing.module.ts
+++ proyecto-clase/projects/alumnos-app/src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListadoAlumnosComponent } from './main-components/listado-alumnos/listado-alumnos.component';
 import { PageNotFoundComponent } from './main-components/page-not-found/page-not-found.component';
 const routes: Routes = [
-  { path: '', component: ListadoAlumnosComponent },
+  { path: '', component: ListadoAlumnosComponent, data:{animation: "HomePage"} },
   {
     path: 'professor',
-    loadChildren : './module/professor/professor.module#ProfessorModule'
+    loadChildren: './module/professor/professor.module#ProfessorModule',
+    data:{animation: "ProfessorPage"}
   }, {
     path: 'groups',
-    loadChildren : './module/group/group.module#GroupModule'
+    loadChildren: './module/group/group.module#GroupModule',
+    data:{animation: "GroupPage"}
   },
   {
-    path: '**' , component:PageNotFoundComponent
+    path: '**', component: PageNotFoundComponent,
+    data:{animation: "NotFoundPage"}
   }
 
 ];
